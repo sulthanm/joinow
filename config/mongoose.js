69aa74dac16018afa0392ccf1647cdbf2bf6c4c8
sulthanm <sulthanso73@gmail.com>
2021-06-14 13:13:21 +0200

    using mongoAtlas at production level

--- config/mongoose.js
+++ config/mongoose.js
@@ -4,7 +4,7 @@ const env = require('./environment');
 // mongoose.connect(`mongodb://localhost/example`);
 
 const MONGODB_URI = `mongodb+srv://sulthanmogal:Sulthan7866129@cluster0.0yfoq.mongodb.net/joinow_development?retryWrites=true&w=majority`;
-mongoose.connect(process.env.MONGODB_URI || MONGODB_URI,
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/joinow_dev',
     { 
         useNewUrlParser: true ,
         useUnifiedTopology: true
