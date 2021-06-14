3e17412e35917218856ff3db3bd45c3abeca0836
sulthanm <sulthanso73@gmail.com>
2021-06-14 12:36:58 +0200

    updating files to deploy

--- config/mongoose.js
+++ config/mongoose.js
@@ -3,7 +3,7 @@ const env = require('./environment');
 
 // mongoose.connect(`mongodb://localhost/example`);
 
-const MONGODB_URI = `mongodb+srv://sulthanmogal:Sulthan7866129@cluster0.0yfoq.mongodb.net/${env.db}?retryWrites=true&w=majority`;
+const MONGODB_URI = `mongodb+srv://sulthanmogal:Sulthan7866129@cluster0.0yfoq.mongodb.net/joinow_development?retryWrites=true&w=majority`;
 mongoose.connect(process.env.MONGODB_URI || MONGODB_URI,
     { 
         useNewUrlParser: true ,
