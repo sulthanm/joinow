aff37df2b02753209c6f41ffaaaffde11b62bea2
sulthanm <sulthanso73@gmail.com>
2021-06-15 08:06:49 +0200

    assets are compressing using gulp

--- config/view-helpers.js
+++ config/view-helpers.js
@@ -8,8 +8,8 @@ module.exports = (app) =>{
             // console.log("------------------",filePath);
             return '/'+filePath;
         }
-        
-        // console.log('/'+JSON.parse(fs.readFileSync(path.join(__dirname,'../public/assets/rev-manifest.json')))[filePath],"dif",filePath);
-        return  '/'+ JSON.parse(fs.readFileSync(path.join(__dirname,'../public/assets/rev-manifest.json')))[filePath];
+        // console.log(filePath,"fsdujkhbsdkuj",'/' + JSON.parse(fs.readFileSync(path.join(__dirname,'../public/assets/rev-manifest.json')))[filePath]);
+       
+        return  '/' + JSON.parse(fs.readFileSync(path.join(__dirname,'../public/assets/rev-manifest.json')))[filePath];
     }
 }
