ef3a73f682c400683cba46bee99abe856d6e1063
sulthanm <sulthanso73@gmail.com>
2021-06-20 17:00:48 +0200

    adding signUp and signIn routes and modifyng css

--- config/environment.js
+++ config/environment.js
@@ -14,7 +14,7 @@ const accessLogStream = rfs.createStream("file.log", {
 
 const development = {
     name : 'development',
-    asset_path : './assets',
+    asset_path : './public/assets',
     session_cookie : 'blahsomthing',
     db : 'joinow_development',
     smtp : {
--- config/view-helpers.js
+++ config/view-helpers.js
@@ -4,12 +4,12 @@ const path =require('path');
 
 module.exports = (app) =>{
     app.locals.assetPath =function(filePath){
-        if(env.name=='development'){
-            // console.log("------------------",filePath);
-            return '/'+filePath;
-        }
+        // if(env.name=='development'){
+            
+        //     return '/'+filePath;
+        // }
         // console.log(filePath,"fsdujkhbsdkuj",'/' + JSON.parse(fs.readFileSync(path.join(__dirname,'../public/assets/rev-manifest.json')))[filePath]);
-       
+        console.log("------------------",filePath);
         return  '/' + JSON.parse(fs.readFileSync(path.join(__dirname,'../public/assets/rev-manifest.json')))[filePath];
     }
 }
