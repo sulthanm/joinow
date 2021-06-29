4342684732cb568019576920cb908b93eadfdc9f
sulthanm <sulthanso73@gmail.com>
2021-06-29 17:04:07 +0200

    managing view vomments

--- config/view-helpers.js
+++ config/view-helpers.js
@@ -8,9 +8,7 @@ module.exports = (app) =>{
             // console.log("------------------",filePath);
             return '/'+filePath;
         }
-        // console.log(filePath,"fsdujkhbsdkuj",'/' + JSON.parse(fs.readFileSync(path.join(__dirname,'../public/assets/rev-manifest.json')))[filePath]);
-     
-        console.log("------------------",filePath);
+       
         return  '/' + JSON.parse(fs.readFileSync(path.join(__dirname,'../public/assets/rev-manifest.json')))[filePath];
     }
 }
